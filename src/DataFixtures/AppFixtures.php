14f43a778df0ae4b21c9d99ba6bc33fee684f90a
orange <jambonbill@gmail.com>
2023-06-27 18:05:24 +0200

    use App\Factory\AuthorFactory;

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,9 @@
 namespace App\DataFixtures;
 
 use App\Entity\Author;
+use App\Factory\AuthorFactory;
+use App\Factory\UserFactory;
+use App\Factory\QuoteFactory;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -11,13 +14,8 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        /*
-        $author = new Author();
-        $author->setName("Albert Einstein");
-        $author->setBio("Albert etait un con");
-        $author->setCreatedAt(new DateTimeImmutable());
-        $manager->persist($author);
-        $manager->flush();
-        */
+        AuthorFactory::createMany(10);
+        UserFactory::createMany(10);
+        QuoteFactory::createMany(25);
     }
 }
